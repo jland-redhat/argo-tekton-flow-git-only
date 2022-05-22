0ccd7cfbcb65fb361a8bf39bb2532dcd5f8fa49c
Jamie Land <jland@redhat.com>
2022-05-22 11:47:08 -0400

    Cleaning up

--- tekton/scripts/update-app.sh
+++ tekton/scripts/update-app.sh
@@ -2,4 +2,4 @@
 echo "Patching Argo Application..."
 set -x
 oc patch Application/$(params.ARGO_APP_NAME) -p '{"spec":{"source":{"helm":{"parameters":[{"name":"image","value":"$(params.IMAGE)" },
-      {"name":"appName","value":"$(params.APP_NAME)" }]}}}}' --type merge -n $(params.ARGO_NAMESPACE)
+      {"name":"app.name","value":"$(params.APP_NAME)" }]}}}}' --type merge -n $(params.ARGO_NAMESPACE)
